1cbd08cf9bbc293f5f36c492bbed6e436cbdb920
Supun200110 <supunnishantha45@gmail.com>
2025-08-14 00:50:05 +0530

    Fixf:

--- quality/app.py
+++ quality/app.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataset
-df = pd.read_csv("quality/notebooks/model_training.ipynb")
+df = pd.read_csv("D:/streamlit assigment/streamlit/quality/data/WineQT.csv")
 
 # Load model
 with open("model.pkl", "rb") as f:
